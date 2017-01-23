c613ddb73e061369829da7cc52586ccb7c4a9d2d
Jesse Hanner <jahanner7@gmail.com>
2017-01-23 16:37:42 -0600

    Implemented lucky number and random fortune

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import random
 import webapp2
+
+def getRandomFortune():
+    fortunes = [
+    "I see much code in your future",
+    "Eat mo chickn",
+    "You have tamed Python"
+    ]
+    index = random.randint(0, 2)
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = "<strong>" + str(random.randint(1, 100)) + "</strong>"
+        number_sentence = 'Your lucky number is: ' + lucky_number
+        num_paragraph = "<p>" + number_sentence + "</p>"
+
+        cookie_button = "<a href='.'><button>Another fortune please!</button></a>"
+
+        content = header + fortune + num_paragraph + cookie_button
+        self.response.write(content)
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Thanks for nothing...")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+('/', MainHandler),
 ], debug=True)
